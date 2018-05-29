7f00b5ece03e8a362f6fc67cdce46d36b6cc303c
Songjl <yoboygo@gmail.com>
2018-05-29 21:54:43 +0800

    'ant匹配'

--- src/main/java/ml/idream/config/SecurityConfig.java
+++ src/main/java/ml/idream/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","/home","error","/signin","/dy/**","/css/**","/js/**","/images/**","/fonts/**").permitAll()
+                .antMatchers("/","/home","error","/**/signin","/dy/**","/css/**","/js/**","/images/**","/fonts/**").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
