651778d89202d47b07585f63cf987571dbcfcefd
songjianlong <songjianlong@idengyun.com>
2018-06-19 15:56:45 +0800

    随机数

--- src/test/java/ml/idream/TestRandom.java
+++ src/test/java/ml/idream/TestRandom.java
@@ -38,8 +38,7 @@ public class TestRandom {
                     sum += item;
                     ret.add(item);
                 }
-
-                /*
+                
                 if(RandomUtils.nextInt(1,1000) % 2 == 0){
                     if(counter < 2){
                         ++counter;
@@ -48,8 +47,6 @@ public class TestRandom {
                     }
 
                 }
-
-*/
 
             }
 
