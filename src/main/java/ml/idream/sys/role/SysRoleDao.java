990703015b8b8ed9654479d4409eb03a0057ae2f
songjianlong <songjianlong@idengyun.com>
2018-06-20 18:33:10 +0800

    注解版

--- src/main/java/ml/idream/sys/role/SysRoleDao.java
+++ src/main/java/ml/idream/sys/role/SysRoleDao.java
@@ -1,7 +1,9 @@
 package ml.idream.sys.role;
 
+import ml.idream.sys.permission.SysPermission;
 import org.apache.ibatis.annotations.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +12,7 @@ public interface SysRoleDao{
 
     @Select("select * from sys_role")
     @Results({
-            @Result(property = "permissions",column = "id",many = @Many(select = "ml.idream.sys.permission.SysPermissionDao.findByRoleId"))
+            @Result(property = "permissions",column = "id", javaType = List.class,many = @Many(select = "ml.idream.sys.permission.SysPermissionDao.findByRoleId"))
     })
     public List<SysRole> findAll();
 
@@ -24,4 +26,7 @@ public interface SysRoleDao{
     @Options(useGeneratedKeys = true)
     @Insert("insert into sys_role_permissioin (role_id,permission_id) values (#{roleId},#{permissionId})")
     void saveRolePermission(List<Map<String,Object>> rolePermission);
+
+    @Select("Select p.* from sys_permission p join sys_role_permission m on m.permission_id = p.id where m.role_id = #{roleId}")
+    public List<SysPermission> findPermissionByRoleId(@Param("roleId") Long roleId);
 }
