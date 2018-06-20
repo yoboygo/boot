fe44b8cb928c02543712dae0516064d3797e8e9b
songjianlong <songjianlong@idengyun.com>
2018-06-20 17:31:12 +0800

    xml

--- src/main/java/ml/idream/sys/role/SysRoleDao.java
+++ src/main/java/ml/idream/sys/role/SysRoleDao.java
@@ -4,7 +4,9 @@ import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 import java.util.Map;
-
+/*
+* @linke {ml.idream.sys.permission.SysPermissionDao.findByRoleId}
+* */
 @Mapper
 public interface SysRoleDao{
 
