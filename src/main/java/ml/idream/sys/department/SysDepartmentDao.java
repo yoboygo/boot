8dfc5855448e14ffcd6305ac1829dba0a6dd0d88
songjianlong <songjianlong@idengyun.com>
2018-06-21 18:37:02 +0800

    xml

--- src/main/java/ml/idream/sys/department/SysDepartmentDao.java
+++ src/main/java/ml/idream/sys/department/SysDepartmentDao.java
@@ -1,16 +1,10 @@
 package ml.idream.sys.department;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface SysDepartmentDao {
-    @Delete("delete from sys_department")
     void deleteAll();
 
-    @Options(useGeneratedKeys = true)
-    @Insert("insert into sys_department (name) values (#{name})")
     void save(SysDepartment department);
 }
--- src/main/java/ml/idream/sys/user/SysUserDao.java
+++ src/main/java/ml/idream/sys/user/SysUserDao.java
@@ -1,11 +1,8 @@
 package ml.idream.sys.user;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.*;
 import org.mapstruct.Mapper;
 
-import java.text.MessageFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -15,52 +12,15 @@ import java.util.Map;
 @Mapper
 public interface SysUserDao{
 
-    @Select("select * from sys_user where name like %${name}% order by id limit 0,1")
-    SysUser findByNameLikeOrderByIdAsc(String name);
-
-    @Select("select * from sys_user where createdate < #{date}")
-    List<SysUser> getByCreatedateLessThan(Date date);
-
-    @Select("select * from sys_user where name = #{name}")
     SysUser findByNameEquals(String name);
 
     @Options(useGeneratedKeys = true)
-    @Insert("insert into sys_user (name,password) values (#{name},#{password})")
+    @Insert("")
     void save(SysUser user);
 
     @Options(useGeneratedKeys = true)
-    @InsertProvider(type = UserDaoProvider.class,method = "insertUserRole")
     void saveUserRole(List<Map<String,Object>> userRole);
 
     @Delete("delete from sys_user")
     void deleteAll();
-}
-
-class UserDaoProvider{
-    public String insertUserRole(Map<String,Object> param){
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO SYS_USER_ROLE ");
-
-        List<Map<String,Object>> list = (List<Map<String, Object>>) param.get("list");
-        boolean isColume = false;
-        if(list.size() <= 0){
-            return "";
-        }
-        sb.append("(");
-        Map<String,Object> first = list.get(0);
-        String formatStr = "(";
-        for(Map.Entry entry : first.entrySet()){
-            sb.append(entry.getKey()).append(",");
-            formatStr += "#{list[{0}]." + entry.getKey() + "},";
-        }
-        formatStr = StringUtils.substringBeforeLast(formatStr,",");
-        formatStr += ") ";
-        sb.append(") VALUES ");
-        MessageFormat mf = new MessageFormat(formatStr);
-        for(int i = 0; i < list.size(); ++i){
-            sb.append(mf.format(new Object[]{i})).append(",");
-        }
-        return sb.substring(0,sb.capacity());
-    }
 }
