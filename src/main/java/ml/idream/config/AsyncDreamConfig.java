aea2485468a01880a7875b9ff141c922dcad6cac
songjianlong <songjianlong@idengyun.com>
2018-07-09 10:16:09 +0800

    将线程池换成TaskThreadPool

--- src/main/java/ml/idream/config/AsyncDreamConfig.java
+++ src/main/java/ml/idream/config/AsyncDreamConfig.java
@@ -3,20 +3,20 @@ package ml.idream.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Configuration
 public class AsyncDreamConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
-       /* ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(5);
-        executor.setThreadNamePrefix("MyDream-");*/
-        ExecutorService executor = Executors.newFixedThreadPool(10);
+        executor.setThreadNamePrefix("MyDream-");
+        executor.initialize();
+//        ExecutorService executor = Executors.newFixedThreadPool(10);
         return executor;
     }
 }
