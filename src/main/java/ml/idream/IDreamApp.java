7af5ec4d5168a33fc12d12a2e31d13683d57e967
songjianlong <songjianlong@idengyun.com>
2018-07-31 15:20:10 +0800

    调整新页面

--- src/main/java/ml/idream/IDreamApp.java
+++ src/main/java/ml/idream/IDreamApp.java
@@ -11,9 +11,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 * Spring boot MainApp
 * */
 @EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ml.idream"})
 @EnableTransactionManagement
-@ComponentScan(basePackages = "ml.idream")
 public class IDreamApp extends AbstractSecurityWebApplicationInitializer {
 
     public static void main(String[] args){
